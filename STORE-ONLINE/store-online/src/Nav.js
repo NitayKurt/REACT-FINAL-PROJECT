ce86c0690ec8d29abf503337d1d259ffd9663251
NitayKurt <nitay121@gmail.com>
2023-01-04 14:13:16 +0200

    new

---
+++ STORE-ONLINE/store-online/src/Nav.js
@@ -0,0 +1,24 @@
+// This file will contain all links:
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function Nav() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-info">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          {/* http://localhost:3000/ */}
+          <li className="nav-item"> <Link className="nav-link" to='/'>HOME</Link> </li>
+          {/* http://localhost:3000/about */}
+          <li className="nav-item"> <Link className="nav-link" to='/about'>ABOUT</Link> </li>
+          {/* http://localhost:3000/items */}
+          <li className="nav-item"> <Link className="nav-link" to='/items'>ITEMS</Link> </li>
+          {/* http://localhost:3000/contactus */}
+          <li className="nav-item"> <Link className="nav-link" to='/contactus'>CONTACT US</Link> </li>
+        </ul>
+      </nav>
+      {/* Renders the child route's element, if there is one. */}
+      <Outlet />
+    </>
+  )
+}
