3fadcfdf1d74c1699f7e1566e9dce144daad109d
NitayKurt <nitay121@gmail.com>
2023-01-05 17:11:30 +0200

    fds

--- STORE-ONLINE/store-online/src/screens/Products.js
+++ STORE-ONLINE/store-online/src/screens/Products.js
@@ -10,7 +10,7 @@ export default function Products() {
     <tr>
       <th scope="col">
               <div className='card' style={{ width: '20rem' }} >
-                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/14/05/21/children-1822688_960_720.jpg" alt='kids playing outside' height={"300px"} width={"300px"}/>
+                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2013/02/12/19/50/lesotho-80841_960_720.jpg" alt='kids playing outside' height={"300px"} width={"350px"}/>
                  <div className="card-body">
                   <h5 className="card-title">OUTSIDE</h5>
                   <p className="card-text">Playing outside with our products</p>
@@ -23,7 +23,7 @@ export default function Products() {
       </th>
       <th scope="col">
               <div className='card' style={{ width: '20rem' }} >
-                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/28/10/48/child-1864718_960_720.jpg" alt='kids playing inside'height={"300px"} width={"300px"}/>
+                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/28/10/48/child-1864718_960_720.jpg" alt='kids playing inside'height={"300px"} width={"350px"}/>
                  <div className="card-body">
                   <h5 className="card-title">INSIDE</h5>
                   <p className="card-text">Playing indoor with our products</p>
@@ -36,7 +36,7 @@ export default function Products() {
       </th>
       <th scope="col">
               <div className='card' style={{ width: '20rem' }} >
-                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2013/12/17/20/10/bubbles-230014_960_720.jpg" alt='kids playing underwater'height={"300px"} width={"300px"}/>
+                <img className="card-img-top" src="https://cdn.pixabay.com/photo/2013/12/17/20/10/bubbles-230014_960_720.jpg" alt='kids playing underwater'height={"300px"} width={"350px"}/>
                  <div className="card-body">
                   <h5 className="card-title">UNDERWATER</h5>
                   <p class="card-text">Playing underwater with us</p>
