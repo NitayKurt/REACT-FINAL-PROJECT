4132f8bcf0797a008e456576a5f00247a731e430
NitayKurt <nitay121@gmail.com>
2023-01-05 12:37:17 +0200

    update bootstarp

--- STORE-ONLINE/store-online/src/index.js
+++ STORE-ONLINE/store-online/src/index.js
@@ -8,7 +8,6 @@ import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 // 1. import the BrowserRouter
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
