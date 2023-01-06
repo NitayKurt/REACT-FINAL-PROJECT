dd1a3116912f47727eb61a2e841871c94425f8f2
NitayKurt <nitay121@gmail.com>
2023-01-06 12:05:01 +0200

    new

--- STORE-ONLINE/store-online/src/screens/products/Inside.js
+++ STORE-ONLINE/store-online/src/screens/products/Inside.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import ProductsList from '../../components/ProductsList'
 
 export default function Inside() {
   return (
-    <div>Inside
-      <ProductsList/>
-    </div>
+    <div>Inside</div>
   )
 }
