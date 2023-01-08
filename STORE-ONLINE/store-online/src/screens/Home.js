480431a9c8d75ad87b52abe3732b052212855943
EviatarZilberman <esztbk@gmail.com>
2023-01-08 20:23:05 +0200

    new Home!

--- STORE-ONLINE/store-online/src/screens/Home.js
+++ STORE-ONLINE/store-online/src/screens/Home.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>HOME</div>
+          <>
+            <div className="App">
+                <h2 className="h2">Welcome To Ariel University Toys Store</h2>
+                <img height="800px" width="600px" src='https://i0.wp.com/brickshow.com/wp-content/uploads/2018/10/71040-1.jpg?resize=627%2C1024&ssl=1' />
+            </div>
+        </>
   )
 }
