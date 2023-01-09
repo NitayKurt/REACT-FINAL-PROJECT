6e521447d8f321e83e51a7966cdcdbdf55afe972
EviatarZilberman <esztbk@gmail.com>
2023-01-09 21:19:00 +0200

    new footer navbar

--- STORE-ONLINE/store-online/src/screens/About.js
+++ STORE-ONLINE/store-online/src/screens/About.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+    <h1 text-align='center'>About Our Store</h1>   <p>Our store were made by a vision. A pretty greedy vision of getting a high grade in React course and improving our gpa. That's it. Thank you for reading.</p>
+      <img height="200px" width="200px" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU_Ug7NwLA-GYwCZy9-ZMpwi8L-r8hj8pZPg&usqp=CAU' />
+  
+  </div>
   )
 }
