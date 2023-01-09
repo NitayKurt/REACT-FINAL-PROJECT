a5261cfd2b2f51944282c2229a08656c8d5df761
EviatarZilberman <esztbk@gmail.com>
2023-01-09 21:20:39 +0200

    new footernavbar

---
+++ STORE-ONLINE/store-online/src/FooterNavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FooterNavBar () {
+  return (
+    <div>
+        <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+          © 2023 Copyrights: <img height="10px" width="10px" src='https://gbiomed.kuleuven.be/english/research/50000715/50000716/ungap/Pictures_and_Logos_UNGAP_website/social-media-logos/linkedin-logo/image' />
+     <a href = 'https://www.linkedin.com/in/nitay-kurt'>Nitay Kurt</a>, <img height="10px" width="10px" src='https://gbiomed.kuleuven.be/english/research/50000715/50000716/ungap/Pictures_and_Logos_UNGAP_website/social-media-logos/linkedin-logo/image' /><a href = 'https://www.linkedin.com/in/eviatar-zilberman-a849891b2/'>Eviatar Zilberman</a>
+        </div>
+    </div>
+  )
+}
